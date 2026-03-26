8c36f10784980a513d3dc61c957bb80152dfd564
RoadSense App <user@roadsense.dev>
2026-03-26 06:50:53 +0000

    Add Mapbox Blue Dot with orientation and tracking

---
+++ public/sw.js
@@ -0,0 +1,13 @@
+const CACHE_NAME = 'roadsense-v1';
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    fetch(event.request).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
